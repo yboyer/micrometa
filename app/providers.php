82bc70aeb6cb75ccfa766c898cd700bd808f38ba
yboyer <bhyw180@gmail.com>
2016-12-09 23:46:01 +0100

    Update root pathes
    Add a base tag to auto match with subfolders other than `/`

--- app/providers.php
+++ app/providers.php
@@ -23,4 +23,7 @@ $app->register(new TranslationServiceProvider(), [
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
 ]);
-$app['twig']->addGlobal('root', '/');
+if (( $rootPath = dirname($_SERVER['SCRIPT_NAME']) ) !== '/') {
+    $rootPath .= '/';
+}
+$app['twig']->addGlobal('root', $rootPath);
